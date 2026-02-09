1a74f667ed24b39e4324c55ccf48bdf4bf40c11d
Mahmut Miftaroski <miftarosky@hotmail.com>
2026-02-09 22:59:36 +0100

    fix: remove unnecessary delay in username and password input fields

--- src/auth.ts
+++ src/auth.ts
@@ -12,8 +12,8 @@ export async function login() {
   const page = await browser.newPage();
   await page.goto("https://courses.finki.ukim.mk/login/index.php");
 
-  await page.type("#username", process.env.FINKI_INDEKS!, { delay: 100 });
-  await page.type("#password", process.env.FINKI_PASSWORD!, { delay: 100 });
+  await page.type("#username", process.env.FINKI_INDEKS!);
+  await page.type("#password", process.env.FINKI_PASSWORD!);
   await page.click(".btn-submit");
   await page.waitForNavigation();
   return { browser, page };
